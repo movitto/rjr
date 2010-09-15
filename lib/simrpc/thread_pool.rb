1e55de3948852b40747c7613ddc724d5581f9ac5
Mohammed Morsi <movitto@yahoo.com>
2010-09-15 02:58:29 -0400

    thread pool fixes

--- lib/simrpc/thread_pool.rb
+++ lib/simrpc/thread_pool.rb
@@ -38,50 +38,64 @@ module Simrpc
     end
   end
 
-  # ActiveObject pattern, encapsulate each thread pool thread in object
-  class ThreadPoolJobRunner
-    attr_accessor :time_started
-
-    def initialize(thread_pool)
-      @thread_pool = thread_pool
-    end
-
-    def run
-      @thread = Thread.new {
-        until @thread_pool.terminate
-          work = @thread_pool.next_job
-          @time_started = Time.now
-          work.handler.call *work.params unless work.nil?
-          @time_started = nil
-        end
-      }
-    end
-
-    def stop
-      @thread.kill
-      @thread.join
-    end
-
-    def join
-      @thread.join
-    end
-  end
 
   # Launches a specified number of threads on instantiation,
   # assigning work to them as it arrives
   class ThreadPool
-    attr_accessor :terminate
+
+    # Encapsulate each thread pool thread in object
+    class ThreadPoolJobRunner
+      attr_accessor :time_started
+
+      def initialize(thread_pool)
+        @thread_pool = thread_pool
+        @timeout_lock = Mutex.new
+        @thread_lock  = Mutex.new
+      end
+
+      def run
+        @thread_lock.synchronize {
+          @thread = Thread.new {
+            until @thread_pool.terminate
+              @timeout_lock.synchronize { @time_started = nil }
+              work = @thread_pool.next_job
+              @timeout_lock.synchronize { @time_started = Time.now }
+              work.handler.call *work.params unless work.nil?
+            end
+          }
+        }
+      end
+
+      # TODO should not invoke after stop is called
+      def check_timeout(timeout)
+        @timeout_lock.synchronize {
+          if !@time_started.nil? && Time.now - @time_started > timeout
+            stop
+            run
+          end
+        }
+      end
+
+      def stop
+        @thread_lock.synchronize {
+          if @thread.alive?
+            @thread.kill
+            @thread.join
+          end
+        }
+      end
+    end
 
     # Create a thread pool with a specified number of threads
     def initialize(num_threads, args = {})
       @num_threads = num_threads
       @timeout     = args[:timeout]
       @job_runners = []
-      @work_queue  = []
-      @work_queue_lock = Mutex.new
-      @work_queue_cv = ConditionVariable.new
-
+      @job_runners_lock = Mutex.new
       @terminate = false
+      @terminate_lock = Mutex.new
+
+      @work_queue  = Queue.new
 
       0.upto(@num_threads) { |i| 
         runner = ThreadPoolJobRunner.new(self)
@@ -92,46 +106,44 @@ module Simrpc
       # optional timeout thread
       unless @timeout.nil?
         @timeout_thread = Thread.new {
-          until @terminate
+          until terminate
             sleep @timeout
-            @job_runners.each { |jr|
-              if !jr.time_started.nil? && (Time.now - jr.time_started > @timeout)
-                jr.stop
-                jr.run
-              end
+            @job_runners_lock.synchronize {
+              @job_runners.each { |jr|
+                jr.check_timeout(@timeout)
+              }
             }
           end
         }
       end
     end
 
-    # Add work to the pool
-    def <<(work)
-      @work_queue_lock.synchronize {
-        @work_queue << work
-        @work_queue_cv.signal
-      }
+    # terminate reader
+    def terminate
+      @terminate_lock.synchronize { @terminate }
     end
 
-    # Return the next job queued up, blocking until one is received 
-    # if none are present
+    # terminate setter
+    def terminate=(val)
+      @terminate_lock.synchronize { @terminate = val }
+    end
+
+    # Add work to the pool
+    def <<(work)
+      @work_queue.push work
+    end
+
+    # Return the next job queued up
     def next_job
-      work = nil
-      @work_queue_lock.synchronize {
-        # wait until we have work
-        @work_queue_cv.wait(@work_queue_lock) if @work_queue.empty?
-        work = @work_queue.shift
-      }
-      work
+      @work_queue.pop
     end
 
     # Terminate the thread pool
     def stop
-      @terminate = true
-      @work_queue_lock.synchronize {
-        @work_queue.clear
-      }
-      @job_runners.each { |jr| jr.stop }
+      terminate = true
+      @timeout_thread.join unless @timout_thread.nil?
+      @work_queue.clear
+      @job_runners_lock.synchronize { @job_runners.each { |jr| jr.stop } }
     end
   end
 end
