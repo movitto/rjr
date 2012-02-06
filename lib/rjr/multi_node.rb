907870156ed28d21b178624d1c98d1f317e2f9b3
Mo Morsi <mmorsi@redhat.com>
2012-02-05 19:01:19 -0500

    add multi-node support for listening on multiple protocols

---
+++ lib/rjr/multi_node.rb
@@ -0,0 +1,31 @@
+# RJR MultiNode Endpoint
+#
+# Copyright (C) 2012 Mohammed Morsi <mo@morsi.org>
+# Licensed under the AGPLv3+ http://www.gnu.org/licenses/agpl.txt
+
+# establish client connection w/ specified args and invoke block w/ 
+# newly created client, returning it after block terminates
+
+require 'eventmachine'
+
+module RJR
+
+class MultiNode < RJR::Node
+  # initialize the node w/ the specified params
+  def initialize(args = {})
+    super(args)
+    @nodes = args[:nodes]
+  end
+
+
+  # Instruct Node to start listening for and dispatching rpc requests
+  def listen
+    em_run do
+      @nodes.each { |node|
+        node.listen
+      }
+    end
+  end
+end
+
+end
