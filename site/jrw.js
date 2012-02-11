1b9ce930e891bde9495919327c105d46507da199
Mo Morsi <mmorsi@redhat.com>
2012-02-11 04:44:54 -0500

    add javascript client for the rjr web interface

--- site/jrw.js
+++ site/jrw.js
@@ -39,8 +39,8 @@ JRObject.from_json = function(json){
   return json['data'];
 };
 
-// main json-rpc interface
-function JRNode (host, port){
+// main json-rpc websocket interface
+function WSNode (host, port){
   node = this;
   this.open = function(){
     this.socket = new MozWebSocket("ws://" + host + ":" + port);
@@ -77,6 +77,8 @@ function JRNode (host, port){
                params: args,
                id: id};
     this.onmessage = function(msg){
+      if(this.message_received)
+        this.message_received(msg);
       if(msg['id'] == id){
         success = !msg['error'];
         if(success && this.onsuccess){
@@ -87,11 +89,61 @@ function JRNode (host, port){
         }
         else if(!success && this.onfailed)
           this.onfailed(msg['error']['code'], msg['error']['message']);
+      }else{
+        if(msg['method'] && this.invoke_callback){
+          params = msg['params'];
+          for(i=0;i<params.length;++i)
+            if(JRObject.is_jrobject(params[i]))
+              params[i] = JRObject.from_json(params[i]);
+          this.invoke_callback(msg['method'], params);
+        }
       }
     };
     this.socket.send($.toJSON(request));
   };
   this.close = function(){
     this.socket.close();
+  };
+};
+
+// main json-rpc www interface
+function WebNode (uri){
+  node = this;
+  this.invoke_request = function(){
+    id = guid();
+    rpc_method = arguments[0];
+    args = [];
+    for(a = 1; a < arguments.length; a++){
+      if(getObjectClass(arguments[a]) == "JRObject")
+        args.push(arguments[a].to_json());
+      else
+        args.push(arguments[a]);
+    }
+    request = {jsonrpc:  '2.0',
+               method: rpc_method,
+               params: args,
+               id: id};
+
+    $.ajax({type: 'POST',
+            url: uri,
+            data: $.toJSON(request),
+            dataType: 'json',
+            success: function(data){
+              if(node.message_received)
+                node.message_received(data);
+              success = !data['error'];
+              if(success && node.onsuccess){
+                result = data['result'];
+                if(JRObject.is_jrobject(result))
+                  result = JRObject.from_json(result);
+                node.onsuccess(result);
+              }
+              else if(!success && node.onfailed)
+                node.onfailed(data['error']['code'], data['error']['message']);
+            },
+            error: function(jqXHR, textStatus, errorThrown){
+              if(node.onfailed)
+                node.onfailed(jqXHR.status, textStatus);
+            }});
   };
 };
