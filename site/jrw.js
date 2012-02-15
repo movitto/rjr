5ac9b90e0f14145d3b9d879881fbf6ed17340615
Mo Morsi <mmorsi@redhat.com>
2012-02-15 08:07:24 -0500

    small variable scope fix to javascript client

--- site/jrw.js
+++ site/jrw.js
@@ -64,11 +64,11 @@ JRObject.is_jrobject = function(json){
 JRObject.from_json = function(json){
   // TODO lookup class corresponding to json['json_class'] in global registry and instantiate
   var obj = json['data'];
-  for(p in obj){
+  for(var p in obj){
     if(JRObject.is_jrobject(obj[p]))
       obj[p] = JRObject.from_json(obj[p]);
     else if(typeof(obj[p]) == "object"){ // handle arrays
-      for(i in obj[p])
+      for(var i in obj[p])
         if(JRObject.is_jrobject(obj[p][i]))
           obj[p][i] = JRObject.from_json(obj[p][i]);
    }
