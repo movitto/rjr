070ffdb28ac9d6356a26a6286387d4eeadd4355e
Mo Morsi <mo@morsi.org>
2012-07-27 05:46:03 -0400

    fix load paths in rjr.rb

--- lib/rjr.rb
+++ lib/rjr.rb
@@ -3,18 +3,15 @@
 # Copyright (C) 2010 Mohammed Morsi <movitto@yahoo.com>
 # Licensed under the AGPLv3+ http://www.gnu.org/licenses/agpl.txt
 
-lib = File.dirname(__FILE__)
-$: << lib + '/rjr/'
-
-require lib + '/rjr/common'
-require lib + '/rjr/errors'
-require lib + '/rjr/thread_pool'
-require lib + '/rjr/semaphore'
-require lib + '/rjr/node'
-require lib + '/rjr/dispatcher'
-require lib + '/rjr/message'
-require lib + '/rjr/local_node'
-require lib + '/rjr/amqp_node'
-require lib + '/rjr/ws_node'
-require lib + '/rjr/web_node'
-require lib + '/rjr/multi_node'
+require 'rjr/common'
+require 'rjr/errors'
+require 'rjr/thread_pool'
+require 'rjr/semaphore'
+require 'rjr/node'
+require 'rjr/dispatcher'
+require 'rjr/message'
+require 'rjr/local_node'
+require 'rjr/amqp_node'
+require 'rjr/ws_node'
+require 'rjr/web_node'
+require 'rjr/multi_node'
