7eca31cae450e7e90268448881dc17df14992000
Mo Morsi <mo@morsi.org>
2014-04-04 07:34:43 -0400

    Update / round out request & result specs

--- specs/request_spec.rb
+++ specs/request_spec.rb
@@ -2,84 +2,145 @@ require 'rjr/request'
 
 module RJR
   describe Request do
-    it "should be convertable to json" do
-      req = Request.new :rjr_method => 'foobar',
-                        :rjr_method_args => [:a, :b],
-                        :rjr_headers => { :header1 => :val1 },
-                        :rjr_node_type => :local,
-                        :rjr_node_id => :loc1
+    describe "#initialize" do
+      it "sets rjr method" do
+        request = Request.new :rjr_method => 'foobar'
+        request.rjr_method.should == 'foobar'
+      end
 
-      res = RJR::Result.new :result => 42,
-                            :error_code => 123,
-                            :error_msg  => 'error occurred',
-                            :error_class => 'ArgumentError'
-      req.result = res
+      it "sets rjr method args" do
+        args = ['a', 'r', 'g']
+        request = Request.new :rjr_method_args => args
+        request.rjr_method_args.should == args
+      end
 
-      j = req.to_json()
-      j.should include('"json_class":"RJR::Request"')
-      j.should include('"rjr_method":"foobar"')
-      j.should include('"rjr_method_args":["a","b"]')
-      j.should include('"rjr_headers":{"header1":"val1"}')
-      j.should include('"rjr_node_type":"local"')
-      j.should include('"rjr_node_id":"loc1"')
-      j.should include('"result":42')
-      j.should include('"error_code":123')
-      j.should include('"error_msg":"error occurred"')
-      j.should include('"error_class":"ArgumentError"')
+      it "sets rjr headers" do
+        headers = {:h => :s}
+        request = Request.new :rjr_headers => headers
+        request.rjr_headers.should == headers
+      end
+
+      it "sets client ip" do
+        request = Request.new :rjr_client_ip => '127.0.0.1'
+        request.rjr_client_ip.should == '127.0.0.1'
+      end
+
+      it "sets client port" do
+        request = Request.new :rjr_client_port => 1234
+        request.rjr_client_port.should == 1234
+      end
+
+      it "sets callback" do
+        request = Request.new :rjr_callback => :cb
+        request.rjr_callback.should == :cb
+      end
+
+      it "sets node" do
+        request = Request.new :rjr_node => :node
+        request.rjr_node.should == :node
+      end
+
+      it "sets node id" do
+        request = Request.new :rjr_node_id => :node_id
+        request.rjr_node_id.should == :node_id
+      end
+
+      it "node type" do
+        request = Request.new :rjr_node_type => :node_type
+        request.rjr_node_type.should == :node_type
+      end
+
+      it "rjr handler" do
+        request = Request.new :rjr_handler => :handler
+        request.rjr_handler.should == :handler
+      end
+
+      it "initialies new RJR::Arguments object from argument list" do
+        args = ['a', 'r', 'g']
+        request = Request.new :rjr_method_args => args
+        request.rjr_method_args.should == args
+        request.rjr_args.should be_an_instance_of(Arguments)
+        request.rjr_args.args.should == args
+      end
+
+      it "sets default result to nil" do
+        Request.new.result.should be_nil
+      end
     end
 
-    it "should be convertable from json" do
-      j = '{"json_class":"RJR::Request","data":{"request":{"rjr_method":"foobar","rjr_method_args":["a","b"],"rjr_headers":{"foo":"bar"},"rjr_node_type":"local","rjr_node_id":"loc1"},"result":{"result":42,"error_code":null,"error_msg":null,"error_class":null}}}'
-      r = JSON.parse(j, :create_additions => true)
-
-      r.class.should == RJR::Request
-      r.rjr_method.should == 'foobar'
-      r.rjr_method_args.should == ['a', 'b']
-      r.rjr_headers.should == { 'foo' => 'bar' }
-      r.rjr_node_type.should == 'local'
-      r.rjr_node_id.should == 'loc1'
-      r.result.result.should == 42
-    end
-
-    context "handling" do
-      it "should invoke handler in request context" do
-        method = 'foobar'
-        params = ['a', 1]
-        ni = 'test'
-        nt = 'test_type'
-        cb = Object.new
-        headers = {'header1' => 'val1'}
-
-        invoked = ip1 = ip2 = 
-        icb = im = ini = int = ih = nil
-        handler = proc { |p1, p2|
-          invoked = true
-          ip1     = p1
-          ip2     = p2
-          im      = @rjr_method
-          ini     = @rjr_node_id
-          int     = @rjr_node_type
-          icb     = @rjr_callback
-          ih      = @rjr_headers
-        }
-
-        request = Request.new :rjr_method      => method,
-                              :rjr_method_args => params,
-                              :rjr_headers     => headers,
-                              :rjr_callback    => cb,
-                              :rjr_node_id     => ni,
-                              :rjr_node_type   => nt,
-                              :rjr_handler     => handler
-
+    describe "#handle" do
+      it "invokes the registered handler with args" do
+        received = nil
+        handler = proc { |arg| received = arg }
+        request = Request.new :rjr_handler => handler,
+                              :rjr_method_args => ['foo']
         request.handle
-        invoked.should be_true
-        ip1.should  == params[0]
-        ip2.should  == params[1]
-        im.should   == method
-        ini.should  == ni
-        int.should  == nt
-        icb.should  == cb
-        ih.should   == headers
+        received.should == 'foo'
+      end
+
+      it "invokes registered handler in request contenxt" do
+        received = nil
+        handler = proc { |arg| received = @var }
+
+        request = Request.new :rjr_handler => handler
+        request.instance_variable_set(:@var, 'foo')
+        request.handle
+        received.should == 'foo'
+      end
+
+      it "returns the handler return value" do
+        handler = proc { |arg| 42 }
+        request = Request.new :rjr_handler => handler
+        request.handle.should == 42
+      end
+    end
+
+    describe "#to_json" do
+      it "returns the request in json format" do
+        request = Request.new :rjr_method => 'foobar',
+                              :rjr_method_args => [:a, :b],
+                              :rjr_headers => { :header1 => :val1 },
+                              :rjr_node_type => :local,
+                              :rjr_node_id => :loc1
+        j = request.to_json
+        j.should include('"json_class":"RJR::Request"')
+        j.should include('"rjr_method":"foobar"')
+        j.should include('"rjr_method_args":["a","b"]')
+        j.should include('"rjr_headers":{"header1":"val1"}')
+        j.should include('"rjr_node_type":"local"')
+        j.should include('"rjr_node_id":"loc1"')
+      end
+
+      it "includes the result in the json" do
+        request = Request.new
+        result = RJR::Result.new :result => 42,
+                                 :error_code => 123,
+                                 :error_msg  => 'error occurred',
+                                 :error_class => 'ArgumentError'
+        request.result = result
+
+        j = request.to_json
+        j.should include('"result":42')
+        j.should include('"error_code":123')
+        j.should include('"error_msg":"error occurred"')
+        j.should include('"error_class":"ArgumentError"')
+      end
+    end
+
+    describe "#json_create" do
+      it "returns a new request from json" do
+        j = '{"json_class":"RJR::Request","data":{"request":{"rjr_method":"foobar","rjr_method_args":["a","b"],"rjr_headers":{"foo":"bar"},"rjr_node_type":"local","rjr_node_id":"loc1"},"result":{"result":42,"error_code":null,"error_msg":null,"error_class":null}}}'
+        request = JSON.parse(j, :create_additions => true)
+
+        request.should be_an_instance_of(RJR::Request)
+        request.rjr_method.should == 'foobar'
+        request.rjr_method_args.should == ['a', 'b']
+        request.rjr_headers.should == { 'foo' => 'bar' }
+        request.rjr_node_type.should == 'local'
+        request.rjr_node_id.should == 'loc1'
+
+        request.result.should be_an_instance_of(RJR::Result)
+        request.result.result.should == 42
       end
     end
   end # describe Request
--- specs/result_spec.rb
+++ specs/result_spec.rb
@@ -39,13 +39,106 @@ module RJR
           result.failed.should  == true
         end
       end
-
     end # describe #initialize
 
     describe "#==" do
-      it "return true for equal results"
-      it "return false for inequal results"
-    end # descirbe #==
+      context "all fields are equal" do
+        it "returns true" do
+          Result.new.should == Result.new
+          Result.new(:result => 1).should == Result.new(:result => 1)
+          Result.new(:error_code => 50).should == Result.new(:error_code => 50)
+        end
+      end
+
+      context "'success' is different" do
+        it "returns false" do
+          r1 = Result.new
+          r2 = Result.new
+          r1.success = true
+          r2.success = false
+          r1.should_not == r2
+        end
+      end
+
+      context "'failed' is different" do
+        it "returns false" do
+          r1 = Result.new
+          r2 = Result.new
+          r1.failed = true
+          r2.failed = false
+          r1.should_not == r2
+        end
+      end
+
+      context "'result' is different" do
+        it "returns false" do
+          r1 = Result.new
+          r2 = Result.new
+          r1.result = 1
+          r2.result = 2
+          r1.should_not == r2
+        end
+      end
+
+      context "'error_code' is different" do
+        it "returns false" do
+          r1 = Result.new
+          r2 = Result.new
+          r1.error_code = 1
+          r2.error_code = 2
+          r1.should_not == r2
+        end
+      end
+
+      context "'error_msg' is different" do
+        it "returns false" do
+          r1 = Result.new
+          r2 = Result.new
+          r1.error_msg = 'something'
+          r2.error_msg = 'bad'
+          r1.should_not == r2
+        end
+      end
+
+      context "'error_class' is different" do
+        it "returns false" do
+          r1 = Result.new
+          r2 = Result.new
+          r1.error_class = 'something'
+          r2.error_class = 'bad'
+          r1.should_not == r2
+        end
+      end
+    end # describe #==
+
+    describe "#invalid_request" do
+      it "is an instance of result" do
+        Result.invalid_request.should be_an_instance_of(Result)
+      end
+
+      it "has error code -32600" do
+        Result.invalid_request.error_code.should == -32600
+      end
+
+      it "has error message 'Invalid Request'" do
+        Result.invalid_request.error_msg.should == "Invalid Request"
+      end
+    end
+
+    describe "#method_not_found" do
+      it "is an instance of result" do
+        Result.method_not_found('foobar').should be_an_instance_of(Result)
+      end
+
+      it "has error code -32602" do
+        Result.method_not_found('foobar').error_code.should == -32602
+      end
+
+      it "has error message 'Method '<name>' not found'" do
+        Result.method_not_found('foobar').error_msg.should ==
+          "Method 'foobar' not found"
+      end
+    end
 
   end # describe Result
 end # module RJR
