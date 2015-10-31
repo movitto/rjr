b3c94c0685f45c5ec00444a20eadcca54728b169
Jonas Collberg <jonas.collberg@gmail.com>
2015-10-31 14:11:48 +0100

    Improve TCP connection handling
    - Add :opened event.
    
    - Add *args to TCP event handlers, to make it possible to pass the
      connection object to handlers.
    
    - Add connection objects to @connections array also for servers.
    
    - Remove the connection from the Node's @connections array when the
      connection is closed.

--- specs/nodes/tcp_spec.rb
+++ specs/nodes/tcp_spec.rb
@@ -42,6 +42,8 @@ module RJR::Nodes
                 :port => 9987)
       end
 
+      let(:host) { 'jsonrpc://localhost:9987' }
+
       let(:client) { TCP.new }
 
       before(:each) do
@@ -51,7 +53,7 @@ module RJR::Nodes
 
       describe "#invoke" do
         it "should invoke request" do
-          res = client.invoke 'jsonrpc://localhost:9987', 'foobar', 'myparam'
+          res = client.invoke host, 'foobar', 'myparam'
           server.halt.join
           res.should == 'retval'
         end
@@ -59,19 +61,99 @@ module RJR::Nodes
 
       describe "#notify" do
         it "should send notification" do
-          res = client.notify 'jsonrpc://localhost:9987', 'foobar', 'myparam'
+          res = client.notify host, 'foobar', 'myparam'
           server.halt.join
           res.should == nil
         end
       end
 
-      describe "closed event handler" do
-        it "should be invoked when connection is closed" do
-          handler_invoked = false
-          server.on(:closed) { |node| handler_invoked = true }
-          client.invoke 'jsonrpc://localhost:9987', 'foobar', 'myparam'
+      describe "a node's connections array" do
+        it "should be updated when a connection is opened" do
+          client.invoke host, 'foobar', 'myparam'
+          server.connections.size.should be == 1
+          client.connections.size.should be == 1
           server.halt.join
-          handler_invoked.should be_truthy
+        end
+
+        it "should be updated when a connection is closed" do
+          client.invoke host, 'foobar', 'myparam'
+          client.connections.first.unbind
+          server.connections.first.unbind
+          server.connections.should be_empty
+          client.connections.should be_empty
+          server.halt.join
+        end
+
+        context "with a client already connected" do
+          let(:another_client) { TCP.new }
+
+          before(:each) do
+            client.invoke host, 'foobar', 'myparam'
+          end
+
+          it "should be updated when a new connection is opened" do
+            another_client.invoke host, 'foobar', 'myparam'
+            server.connections.size.should be == 2
+            client.connections.size.should be == 1
+            another_client.connections.size.should be == 1
+            server.halt.join
+          end
+
+          context "with two clients connected" do
+            before(:each) do
+              client.invoke host, 'foobar', 'myparam'
+              another_client.invoke host, 'foobar', 'myparam'
+            end
+
+            it "should be updated when a connection is closed" do
+              another_client.connections.first.unbind
+              server.connections.last.unbind
+              server.connections.size.should be == 1
+              client.connections.size.should be == 1
+              another_client.connections.should be_empty
+              server.halt.join
+            end
+          end
+        end
+      end
+
+      describe "event handlers" do
+        shared_examples "an event handler" do |event|
+          it "should be invoked when event is triggered" do
+            handler_invoked = false
+            server.on(event) { handler_invoked = true }
+            client.invoke host, 'foobar', 'myparam'
+            server.halt.join
+            handler_invoked.should be_truthy
+          end
+
+          it "should receive the node" do
+            node_received = false
+            server.on(event) do |node|
+              node_received = true if node.is_a?(RJR::Node)
+            end
+            client.invoke host, 'foobar', 'myparam'
+            server.halt.join
+            node_received.should be_truthy
+          end
+
+          it "should receive the connection" do
+            connection_received = false
+            server.on(event) do |_, connection|
+              connection_received = true if connection.is_a?(EM::Connection)
+            end
+            client.invoke host, 'foobar', 'myparam'
+            server.halt.join
+            connection_received.should be_truthy
+          end
+        end
+
+        describe "opened event handler" do
+          it_behaves_like "an event handler", :opened
+        end
+
+        describe "closed event handler" do
+          it_behaves_like "an event handler", :closed
         end
       end
     end
